c7efce3d40dcb009c46839d1c77084679d0d1207
oeseo <108154043+oeseo@users.noreply.github.com>
2024-10-28 14:30:21 +0500

    Update models.py
    Создал кастомной модели пользователя

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length=255)
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
+    is_dismissed = models.BooleanField(default=False)
+    dismissal_date = models.DateField(null=True, blank=True)
