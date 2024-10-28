417b2483d53a6c379a558fcfd284296a319a0d19
oeseo <108154043+oeseo@users.noreply.github.com>
2024-10-28 14:49:31 +0500

    Update views.py
    Создал API представлений

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['is_dismissed']
+
+    # Дополнительный метод для фильтрации пользователей по статусу
+    @action(detail=False, methods=['get'])
+    def filter_by_dismissed(self, request):
+        is_dismissed = request.query_params.get('is_dismissed', 'false').lower() == 'true'
+        users = CustomUser.objects.filter(is_dismissed=is_dismissed)
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
 
 # Create your views here.
