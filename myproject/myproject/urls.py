775c8055abd390650dbe18cfc25aae9eed8367bc
oeseo <108154043+oeseo@users.noreply.github.com>
2024-10-28 22:16:12 +0500

    Исправил ирлс

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.shortcuts import redirect
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', lambda request: redirect('api', permanent=False)),
+    path('', lambda request: redirect('api-root', permanent=False)),
+    path('api/', include('users.urls'), name='api-root'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/', include('users.urls')),
 ]
